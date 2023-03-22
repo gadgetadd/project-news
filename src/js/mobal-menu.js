82444f7bb482e7443dae516978ad09b127fd402d
Anastasiia <dessertkiev@gmail.com>
2023-03-23 00:26:20 +0200

    header

--- src/js/mobal-menu.js
+++ src/js/mobal-menu.js
@@ -2,6 +2,7 @@
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const menuBtnRef2 = document.querySelector("[data-menu-button2]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const themaMenuRef = document.querySelector("[data-thema]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -14,6 +15,7 @@
     menuBtnRef2.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
+    themaMenuRef.classList.toggle("is-open");
 
   });
 })();
