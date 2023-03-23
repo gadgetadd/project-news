abedd637237375bab896a8274b47add6326f0012
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-03-23 16:14:13 +0200

    addEventListener-click

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -5,10 +5,21 @@ const remove = document.querySelector('#remove-card');
 //   alert('Parent click handler');
 // });
 
-// child.addEventListener('click', () => {
-//   alert('Child click handler');
-// });
+// loadMoreBtn.addEventListener('click', onClickLoadMoreBtn);
 
-// descendant.addEventListener('click', () => {
-//   alert('Descendant click handler');
-// });
+// async function onClickLoadMoreBtn() {
+//   currentPage += 1;
+//   try {
+//     const response = await fetchImages(searchQuery, currentPage);
+//     renderCardImage(response.hits);
+//     lightbox.refresh();
+//     currentHits += response.hits.length;
+
+//     if (currentHits === response.totalHits) {
+//       loadMoreBtn.classList.add('is-hidden');
+//       endCollectionText.classList.remove('is-hidden');
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
