2f046deb1005a11c45d886d508c0ff0f2496ee70
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-03-23 16:03:33 +0200

    document.querySelector

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -1,15 +1,14 @@
 const parent = document.querySelector('#parent');
 const child = document.querySelector('#child');
-const descendant = document.querySelector('#descendant');
 
-parent.addEventListener('click', () => {
-  alert('Parent click handler');
-});
+// parent.addEventListener('click', () => {
+//   alert('Parent click handler');
+// });
 
-child.addEventListener('click', () => {
-  alert('Child click handler');
-});
+// child.addEventListener('click', () => {
+//   alert('Child click handler');
+// });
 
-descendant.addEventListener('click', () => {
-  alert('Descendant click handler');
-});
+// descendant.addEventListener('click', () => {
+//   alert('Descendant click handler');
+// });
