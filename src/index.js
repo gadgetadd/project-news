43b5984e20c828abc4431a8b227ec1956e8e02e1
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-03-24 17:53:48 +0200

    js

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import './js/select-menu';
--- src/js/select-menu.js
+++ src/js/select-menu.js
@@ -1,7 +1,15 @@
 const select = document.querySelector('.select');
 const select__list = document.querySelector('.select__list');
-const options = document.querySelector('.options');
+const options = document.querySelectorAll('.options');
 
 select.addEventListener('clic', () => {
   select__list.classList.toggle('active');
+  // не знаю як тут бути стрілка вниз вверх
+  select
+    .querySelector('.categories__icon')
+    .classList.toggle('.categories__icon');
+});
+// вибір опцій
+options.forEach(option => {
+  option.addEventListener('click', () => {});
 });
