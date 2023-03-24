6ec7124f6761ac949814228de9306b601fec5549
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-03-24 15:26:58 +0200

    add/switch

---
+++ src/js/switch-theme.js
@@ -0,0 +1,51 @@
+const input = document.querySelector('.switch__input');
+const sun = document.querySelector('.icon-sun');
+const moon = document.querySelector('.icon-moon')
+
+
+setBackgroundColor();
+
+input.addEventListener('click', onInputClick)
+function onInputClick(e){
+    let theme
+    if (e.target.checked){
+        theme = 'dark'
+        localStorage.setItem('theme', theme);
+    }
+    else{
+        theme = 'light'
+        localStorage.setItem('theme', theme);
+    }
+    setBackgroundColor(theme);
+}
+
+function setBackgroundColor(e){
+    const theme = localStorage.getItem('theme');
+    if(!theme){
+        return;
+    }
+    if(theme === 'light'){
+        document.querySelector('html').classList.remove('dark')
+        document.body.classList.remove('body-dark')
+    }
+    else{
+        input.checked = true;
+        document.querySelector('html').classList.add('dark') 
+    }
+    switchIcon(e)
+}
+
+function switchIcon(e){
+    if(input.checked){
+        // sun.classList.add('dark-icon')
+        // moon.classList.remove('dark-icon')
+        document.querySelector('p[data-light]').classList.add('checkbox-dark')
+        document.querySelector('p[data-dark]').classList.remove('checkbox-dark')
+    }
+    else{
+        // sun.classList.remove('dark-icon')
+        // moon.classList.add('dark-icon')
+        document.querySelector('p[data-light]').classList.remove('checkbox-dark')
+        document.querySelector('p[data-dark]').classList.add('checkbox-dark')
+    }
+}
