c4fad90f84621c0b923917db9c3b9cb02a52f716
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-03-25 15:29:11 +0200

    js select
    js для випадалки і вибором категорій

--- src/js/select-menu.js
+++ src/js/select-menu.js
@@ -1,7 +1,7 @@
 const select = document.querySelector('.select');
 const select__list = document.querySelector('.select__list');
 const options = document.querySelectorAll('.options');
-
+// виїжає випадалка
 select.addEventListener('click', () => {
   select__list.classList.toggle('active');
   // стрілка вниз вверх
@@ -9,5 +9,13 @@ select.addEventListener('click', () => {
 });
 // вибір опцій
 options.forEach(option => {
-  option.addEventListener('click', () => {});
+  option.addEventListener('click', () => {
+    options.forEach(option => {
+      option.classList.remove('selected');
+    });
+    select.querySelector('span').innerHTML = option.innerHTML;
+    option.classList.add('selected');
+    select__list.classList.toggle('active');
+    select.querySelector('.categories__icon').classList.toggle('inverted');
+  });
 });
