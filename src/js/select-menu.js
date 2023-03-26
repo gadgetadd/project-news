f165266398d6b1c82fe1f75170ac06d031df0081
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-03-26 17:07:11 +0300

    correct section Btn
     відступи, висота кнопки, div class="category-wrapper"

--- src/js/select-menu.js
+++ src/js/select-menu.js
@@ -1,6 +1,7 @@
 const select = document.querySelector('.select');
 const select__list = document.querySelector('.select__list');
 const options = document.querySelectorAll('.options');
+// const categoriesBtn = document.querySelector('');
 // виїжає випадалка
 select.addEventListener('click', () => {
   select__list.classList.toggle('active');
