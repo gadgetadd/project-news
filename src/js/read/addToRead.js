9bc00fe4134df3b9fa84e1d8469dd1ec91830187
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-03-27 15:55:41 +0300

    #10

---
+++ src/js/read/addToRead.js
@@ -0,0 +1,37 @@
+export const addToRead = () => {
+    const AllArticles = document.querySelectorAll('.news__item');
+
+    for (let i = 0; i < AllArticles.length; i += 1) {
+        const el = AllArticles[i];
+
+        el.children[1].children[1].addEventListener('click', (evt) => {
+            evt.preventDefault();
+            const articlesRead = {...(JSON.parse(localStorage.getItem('readArticles')))}
+            const article = {
+                category: el.children[0].children[0].children[1].textContent,
+                date: el.children[1].children[0].attributes[0].value,
+                descr: el.children[0].children[2].innerText,
+                image: el.children[0].children[0].children[0].children[2].attributes[1].value,
+                title: el.children[0].children[1].textContent,
+                url: el.children[1].children[1].attributes[1].value,
+            };
+            
+            if (article.date in articlesRead) {
+                if (availabilityCheck(articlesRead[article.date], article.title)) {
+                    return
+                };
+                articlesRead[article.date].push(article);
+            } else {
+                articlesRead[article.date] = [article];
+            };
+
+            localStorage.setItem('readArticles', (JSON.stringify(articlesRead)));
+        });
+    };
+};
+
+const availabilityCheck = (arr, value) => {
+    return arr.some((el) => {
+        return (el.title === value)
+    }); 
+};
--- src/read.js
+++ src/read.js
@@ -1,5 +1,7 @@
 import './js/mobile-menu.js';
 import './js/switch';
-import { renderPage } from './js/render-read.js';
+import { renderPage } from './js/read/render-read.js';
+import { addToRead } from './js/read/addToRead.js';
 
 renderPage();
+addToRead();
