f76b97dee3b71c6a51699d6712c81c73c7efa57f
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-03-27 11:54:09 +0300

    favorite-news

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './js/mobile-menu.js';
 import './js/select-menu.js';
 import './js/switch';
 import './js/adaptiveRender';
+import './js/favorite';
--- src/js/favorite.js
+++ src/js/favorite.js
@@ -1,21 +1,43 @@
-const add = document.querySelector('#add-card');
-const remove = document.querySelector('#remove-card');
+// Отримати кнопку AddToFavorite
+const addToFavoriteBtn = document.querySelector('.button-card');
+addToFavoriteBtn.addEventListener('click', addToFavorite);
 
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
+function addToFavorite() {
+  // Отримати інформацію про додану новину (наприклад, заголовок)
+  const newsTitle = document.querySelector('.news');
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
+  // Отримати масив новин, які вже додані в обране (якщо такі є)
+  let favoriteNews = JSON.parse(localStorage.getItem('favoriteNews')) || [];
 
-// const onPlay = function (data) {
-//   localStorage.setItem(
-//     'videoplayer-current-time',
-//     JSON.stringify(data.seconds)
-//   );
-// };
+  // Додати нову новину до масиву доданих в обране новин
+  favoriteNews.push(newsTitle);
 
-// player.on('timeupdate', throttle(onPlay, 1000));
+  // Зберегти масив доданих в обране новин в локальному сховищі браузера
+  localStorage.setItem('favoriteNews', JSON.stringify(favoriteNews));
+}
 
-// const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+// Отримати масив доданих в обране новин з локального сховища браузера
+const favoriteNews = JSON.parse(localStorage.getItem('favoriteNews')) || [];
 
-// player.setCurrentTime(currentTime);
+// Отримати контейнер для галереї на сторінці Favorite
+const favoriteGallery = document.querySelector;
+
+function removeFromFavorite(id) {
+  // Отримуємо список збережених новин з локального сховища
+  let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+  // Шукаємо новину за її ідентифікатором
+  const index = favorites.findIndex(item => item.id === id);
+
+  // Якщо новина знайдена, видаляємо її зі списку
+  if (index !== -1) {
+    favorites.splice(index, 1);
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  }
+
+  // Видаляємо картку зі сторінки Favorite
+  const card = document.getElementById(`card-${id}`);
+  if (card) {
+    card.remove();
+  }
+}
