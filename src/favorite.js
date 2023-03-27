ef98aad064328f05b0989a26b17fb6e53f0dfbec
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-03-27 15:59:15 +0300

    #11

--- src/favorite.js
+++ src/favorite.js
@@ -1,2 +1,5 @@
 import './js/mobile-menu.js';
 import './js/switch';
+import { addToRead } from './js/read/addToRead.js';
+
+addToRead();
--- src/read.js
+++ src/read.js
@@ -1,7 +1,5 @@
 import './js/mobile-menu.js';
 import './js/switch';
 import { renderPage } from './js/read/render-read.js';
-import { addToRead } from './js/read/addToRead.js';
 
 renderPage();
-addToRead();
