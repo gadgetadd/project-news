a6d8b0b7045f7db09d0d1c04e4547abd8accddd7
Алексей <electrifyx@gmail.com>
2023-03-27 10:22:36 +0300

    edit card-main.js

--- src/js/card-main.js
+++ src/js/card-main.js
@@ -1,8 +1,8 @@
 import sprite from '../images/icons.svg';
 
 const cardsNewsBoxEl = document.querySelector('.card-wrap');
-const cardNewsEl = document.querySelector('.news');
-const weatherCard = document.querySelector('.weather-card');
+// const cardNewsEl = document.querySelector('.news');
+// const weatherCard = document.querySelector('.weather-card');
 
 const normalizeDate = date => {
   const zero = '0';
@@ -83,9 +83,9 @@ const normalizeDate = date => {
 // };
 
 const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-function renderCardsNews(ar) {
-  return ar.map(el => {
-    const date = new Date(el.date);
+function renderCardsNews(card) {
+  return card.map(element => {
+    const date = new Date(element.date);
     const { day, month, year } = normalizeDate(date);
     return `
           <li class="news__item">
@@ -93,27 +93,27 @@ function renderCardsNews(ar) {
                   <div class="news__img">
                       <picture>
                       <source
-                          srcset="${el.image}"
+                          srcset="${element.image}"
                           media="(max-width: 767px)"
                           width="288"
                           height="395"
                       />
                       <source
-                          srcset="${el.image}"
+                          srcset="${element.image}"
                           media="(max-width: 1279px)"
                           width="353"
                           height="395"
                       />
                       <img
-                          srcset="${el.image}"
-                          src="${el.image}"
+                          srcset="${element.image}"
+                          src="${element.image}"
                           alt=""
                           width="395"
                           height="395"
                       />
                       </picture>
 
-                      <p class="news__category">${el.category}</p>
+                      <p class="news__category">${element.category}</p>
 
                       <button type="button" class="button-card">
                       Add to favorite
@@ -122,14 +122,14 @@ function renderCardsNews(ar) {
                       </svg>
                       </button>
                   </div>
-                  <h1 class="news__title">${el.title}</h1>
+                  <h1 class="news__title">${element.title}</h1>
                   <p class="news__text">
-                      ${el.descr}
+                      ${element.descr}
                   </p>
               </div>
               <div class="news__info">
-                  <time datetime="${el.date}" class="news__time">${day}/${month}/${year}</time>
-                  <a class="news__link" href="${el.url}">Read more</a>
+                  <time datetime="${element.date}" class="news__time">${day}/${month}/${year}</time>
+                  <a class="news__link" href="${element.url}">Read more</a>
               </div>
           </li>`;
   });
