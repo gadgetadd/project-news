9f0bccca8e20d68251ef3c06b298a901bef1d52c
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-03-27 15:32:01 +0300

    favorite-gallery

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -14,47 +14,38 @@ function addToFavorite() {
 
   // Зберегти масив доданих в обране новин в локальному сховищі браузера
   localStorage.setItem('favoriteNews', JSON.stringify(favoriteNews));
+
+  // hasCard = card => favoriteNews.some(({ id }) => id === card.id);
 }
 
 // Відображаємо галерею зі списком збережених новин
 const favoriteGallery = document.querySelector('#favorite-gallery');
 favoriteNews.forEach(news => {
   const newsCard = document.createElement('div');
-  newsCard.innerHTML = `
-    <h2>${news.title}</h2>
-    <p>${news.description}</p>
-    <button data-id="${news.id}" class="remove-btn">RemoveFromFavorite</button>
+  newsCard.innerHTML = ` 
+    // <h2>${news.title}</h2>
+    // <p>${news.description}</p>
+    // <button data-id="${news.id}" class="remove-btn">RemoveFromFavorite</button>
   `;
-  newsGallery.appendChild(newsCard);
+  favoriteGallery.appendChild(newsCard);
 });
 
-// Додаємо обробник подій для видалення новини зі списку збережених
-const removeButtons = document.querySelectorAll('.remove-btn');
-removeButtons.forEach(button => {
-  button.addEventListener('click', event => {
-    const newsId = event.target.dataset.id;
-    const updatedNews = favoriteNews.filter(news => news.id !== newsId);
-    localStorage.setItem('favoriteNews', JSON.stringify(updatedNews));
-    event.target.parentNode.remove();
-  });
-});
+function removeFromFavorite(id) {
+  // Отримуємо список збережених новин з локального сховища
+  let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
-// function removeFromFavorite(id) {
-//   // Отримуємо список збережених новин з локального сховища
-//   let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  // Шукаємо новину за її ідентифікатором
+  const index = favorites.findIndex(item => item.id === id);
 
-//   // Шукаємо новину за її ідентифікатором
-//   const index = favorites.findIndex(item => item.id === id);
+  // Якщо новина знайдена, видаляємо її зі списку
+  if (index !== -1) {
+    favorites.splice(index, 1);
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  }
 
-//   // Якщо новина знайдена, видаляємо її зі списку
-//   if (index !== -1) {
-//     favorites.splice(index, 1);
-//     localStorage.setItem('favorites', JSON.stringify(favorites));
-//   }
-
-//   // Видаляємо картку зі сторінки Favorite
-//   const card = document.getElementById(`card-${id}`);
-//   if (card) {
-//     card.remove();
-//   }
-// }
+  // Видаляємо картку зі сторінки Favorite
+  const card = document.getElementById(`card-${id}`);
+  if (card) {
+    card.remove();
+  }
+}
