aac0d3f2cd66b899a691d398c2251077d18225e6
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-03-27 14:49:24 +0300

    favoriteGallery

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -17,7 +17,7 @@ function addToFavorite() {
 }
 
 // Відображаємо галерею зі списком збережених новин
-const newsGallery = document.querySelector('#news-gallery');
+const favoriteGallery = document.querySelector('#favorite-gallery');
 favoriteNews.forEach(news => {
   const newsCard = document.createElement('div');
   newsCard.innerHTML = `
@@ -39,22 +39,22 @@ removeButtons.forEach(button => {
   });
 });
 
-function removeFromFavorite(id) {
-  // Отримуємо список збережених новин з локального сховища
-  let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+// function removeFromFavorite(id) {
+//   // Отримуємо список збережених новин з локального сховища
+//   let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
-  // Шукаємо новину за її ідентифікатором
-  const index = favorites.findIndex(item => item.id === id);
+//   // Шукаємо новину за її ідентифікатором
+//   const index = favorites.findIndex(item => item.id === id);
 
-  // Якщо новина знайдена, видаляємо її зі списку
-  if (index !== -1) {
-    favorites.splice(index, 1);
-    localStorage.setItem('favorites', JSON.stringify(favorites));
-  }
+//   // Якщо новина знайдена, видаляємо її зі списку
+//   if (index !== -1) {
+//     favorites.splice(index, 1);
+//     localStorage.setItem('favorites', JSON.stringify(favorites));
+//   }
 
-  // Видаляємо картку зі сторінки Favorite
-  const card = document.getElementById(`card-${id}`);
-  if (card) {
-    card.remove();
-  }
-}
+//   // Видаляємо картку зі сторінки Favorite
+//   const card = document.getElementById(`card-${id}`);
+//   if (card) {
+//     card.remove();
+//   }
+// }
