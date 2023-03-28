40577a9d6e1b7020f027e00be4dbc24ab840b1b6
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-03-29 01:24:05 +0300

    Update addToRead.js

--- src/js/addToRead.js
+++ src/js/addToRead.js
@@ -1,66 +1,62 @@
 import sprite from '../images/icons.svg';
 
 const availabilityCheck = (arr, value) => {
-  return arr.some(el => {
-    return el.title === value;
-  });
+    return arr.some((el) => {
+        return (el.title === value)
+    }); 
 };
 
-const alreadyRead = elem => {
-  elem.classList.add('read__already-read');
-  const isRead = `<span class="is-read">
+const alreadyRead = (elem) => {
+    elem.classList.add('read__already-read');
+    const isRead = `<span class="is-read">
                     Already read
                     <svg class="is-read__icon" width="15" height="11">
                         <use href="${sprite}#icon-check"></use>
                     </svg>
                     </span>`;
-  elem.insertAdjacentHTML('beforeend', isRead);
+    elem.insertAdjacentHTML('beforeend', isRead)
 };
 
 export const addToRead = () => {
-  const AllArticles = document.querySelectorAll('.news__item');
-  const articlesRead = { ...JSON.parse(localStorage.getItem('readArticles')) };
+    if (!(localStorage.getItem('readArticles'))) {
+        localStorage.setItem('readArticles', JSON.stringify({}));
+    };
 
-  for (let i = 0; i < AllArticles.length; i += 1) {
-    const el = AllArticles[i];
 
-    for (const key in articlesRead) {
-      if (key === el.children[1].children[0].attributes[0].value) {
-        if (
-          availabilityCheck(
-            articlesRead[key],
-            el.children[0].children[1].textContent
-          )
-        ) {
-          alreadyRead(el);
-        }
-      }
-    }
+    const AllArticles = document.querySelectorAll('.news__item');
+    const articlesRead = { ...(JSON.parse(localStorage.getItem('readArticles'))) };
 
-    el.children[1].children[1].addEventListener('click', evt => {
-      const article = {
-        category: el.children[0].children[0].children[1].textContent,
-        date: el.children[1].children[0].attributes[0].value,
-        descr: el.children[0].children[2].innerText,
-        image:
-          el.children[0].children[0].children[0].children[2].attributes[1]
-            .value,
-        title: el.children[0].children[1].textContent,
-        url: el.children[1].children[1].attributes[1].value,
-      };
+    for (let i = 0; i < AllArticles.length; i += 1) {
+        const el = AllArticles[i];
 
-      if (article.date in articlesRead) {
-        if (availabilityCheck(articlesRead[article.date], article.title)) {
-          return;
-        }
-        articlesRead[article.date].push(article);
-      } else {
-        articlesRead[article.date] = [article];
-      }
+        for (const key in articlesRead) {
+            if (key === el.children[1].children[0].attributes[0].value) {
+                if (availabilityCheck(articlesRead[key], el.children[0].children[1].innerText)) {
+                    alreadyRead(el);
+                };
+            };
+        };
 
-      localStorage.setItem('readArticles', JSON.stringify(articlesRead));
-    });
-  }
+        el.children[1].children[1].addEventListener('click', (evt) => {
+            const article = {
+                category: el.children[0].children[0].children[1].textContent,
+                date: el.children[1].children[0].attributes[0].value,
+                descr: el.children[0].children[2].innerText,
+                image: el.children[0].children[0].children[0].children[0].children[2].attributes[1].value,
+                title: el.children[0].children[1].innerText,
+                url: el.children[1].children[1].attributes[1].value,
+            };
+
+            if (article.date in articlesRead) {
+                if (availabilityCheck(articlesRead[article.date], article.title)) {
+                    return
+                };
+                articlesRead[article.date].push(article);
+            } else {
+                articlesRead[article.date] = [article];
+            };
+
+            localStorage.setItem('readArticles', (JSON.stringify(articlesRead)));
+        });
+    };
 };
-
-addToRead();
