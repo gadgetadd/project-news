1ea0cc09619143b9231bb04a55d78962742acc49
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-03-29 01:08:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './js/categories/addCategories.js';
 import './js/card-main.js';
 import './js/mobile-menu.js';
 import './js/select-menu.js';
-import './js/switch';
+import './js/switch.js';
 import './js/calendar.js';
 import './js/search.js';
 import './js/addToRead';
