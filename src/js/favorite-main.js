867b9f5fd36115fbf65c78959fbfdca95b3b3fec
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-03-28 21:39:19 +0300

    availabilityCheck

--- src/js/favorite-main.js
+++ src/js/favorite-main.js
@@ -1,24 +1,96 @@
-// import sprite from '../images/icons.svg';
+import sprite from '../images/icons.svg';
 import desktop from '../images/no-news-desktop.png';
 import desktopX2 from '../images/no-news-desktop@2x.png';
 import tablet from '../images/no-news-tablet.png';
 import tabletX2 from '../images/no-news-tablet@2x.png';
 import mobile from '../images/no-news-mobile.png';
 import mobileX2 from '../images/no-news-mobile@2x.png';
-// const container = document.querySelector('#art-container');
-const gallery = document.querySelector('.gallery');
 
-import { createCardsMarkup } from './card-main';
+const container = document.querySelector('#box-container');
 
-function displayFavorites() {
-  let favoriteNews = JSON.parse(localStorage.getItem('favoriteNews')) || [];
-  if (favoriteNews.length === 0) return renderEmpty();
-  const cardsMarkup = createCardsMarkup(favoriteNews);
-  gallery.insertAdjacentHTML('afterbegin', cardsMarkup);
-}
+const normalizeDate = date => {
+  const zero = '0';
+  const normalDate = {
+    month: 0,
+    day: 0,
+    year: date.getFullYear(),
+  };
 
-function renderEmpty() {
-  gallery.innerHTML = ` <li><picture>
+  if (date.getMonth() <= 8) {
+    normalDate.month = `${zero}${date.getMonth() + 1}`;
+  } else {
+    normalDate.month = date.getMonth() + 1;
+  }
+
+  if (date.getDate() <= 9) {
+    normalDate.day = `${zero}${date.getDate()}`;
+  } else {
+    normalDate.day = date.getDate();
+  }
+  return normalDate;
+};
+
+const renderArticles = articles => {
+  const list = document.createElement('ul');
+  list.classList.add('news');
+  const articlesHTML = articles.reduce((acc, el) => {
+    const date = new Date(el.date);
+    const { day, month, year } = normalizeDate(date);
+    return (acc += `
+                <li class="news__item">
+                    <div class="news__card">
+                        <div class="news__img">
+                            <picture>
+                            <source
+                                srcset="${el.image}"
+                                media="(max-width: 767px)"
+                                width="288"
+                                height="395"
+                            />
+                            <source
+                                srcset="${el.image}"
+                                media="(max-width: 1279px)"
+                                width="353"
+                                height="395"
+                            />
+                            <img
+                                srcset="${el.image}"
+                                src="${el.image}"
+                                alt=""
+                                width="395"
+                                height="395"
+                            />
+                            </picture>
+
+                            <p class="news__category">${el.category}</p>
+
+                            <button type="button" class="button-card">
+                            Add to favorite
+                            <svg class="button-card-icon" width="16" height="16">
+                                <use href="${sprite}#icons_heart"></use>
+                            </svg>
+                            </button>
+                        </div>
+                        <h1 class="news__title">${el.title}</h1>
+                        <p class="news__text">
+                            ${el.descr}
+                        </p>
+                    </div>
+                    <div class="news__info">
+                        <time datetime="${el.date}" class="news__time">${day}/${month}/${year}</time>
+                        <a class="news__link" href="${el.url}">Read more</a>
+                    </div>
+                </li>`);
+  }, '');
+  list.insertAdjacentHTML('beforeend', articlesHTML);
+  return list;
+};
+
+export const renderPage = () => {
+  const articls = JSON.parse(localStorage.getItem('favoriteArticles'));
+
+  if (articls === null) {
+    container.innerHTML = ` <picture>
                 <source
                     srcset="${mobile} 1x, ${mobileX2} 2x"
                     media="(max-width: 767px)">
@@ -30,8 +102,28 @@ function renderEmpty() {
                     media="(min-width: 1280px)">
                 <img class="read__no-news" src="${mobile}"
                     alt="Зображення не має новин">
-            </picture><li>`;
-}
+            </picture>`;
+    return;
+  }
+
+  const list = document.querySelectorAll('.read__list-wrapper');
+  console.log(list);
+
+  for (let i = 0; i < list.length; i += 1) {
+    list[i].children[0].addEventListener('click', () => {
+      list[i].children[0].children[0].classList.toggle('read--rotate');
+      const clientHeight = list[i].clientHeight;
+      const height = list[i].scrollHeight;
+      if (clientHeight > 33) {
+        list[i].style.height = '33px';
+        return;
+      }
+      list[i].style.height = `${height}px`;
+    });
+  }
+};
+
+renderPage();
 
 // function removeFromFavorite(id) {
 //   // Отримуємо список збережених новин з локального сховища
@@ -52,5 +144,3 @@ function renderEmpty() {
 //     card.remove();
 //   }
 // }
-
-displayFavorites();
