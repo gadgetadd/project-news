d9bb2664791dcefe3f2d8d7fdaa45a7138b836c0
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-03-28 11:55:55 +0300

    1234

--- src/js/addToFavorite.js
+++ src/js/addToFavorite.js
@@ -6,13 +6,13 @@ addToFavoriteBtn.forEach(battons => {
 function addToFavorite() {
   // Отримати інформацію про додану новину
   const newsCard = document.querySelector('.news__item');
-
+  console.log(newsCard);
   // Отримати масив новин, які вже додані в обране (якщо такі є)
   let favoriteNews = JSON.parse(localStorage.getItem('favoriteNews')) || [];
-
+  console.log(favoriteNews);
   // Додати нову новину до масиву доданих в обране новин
-  favoriteNews.push(newsCard);
-
+  let newsArry = favoriteNews.push(newsCard);
+  console.log(newsArry);
   // Зберегти масив доданих в обране новин в локальному сховищі браузера
   localStorage.setItem('favoriteNews', JSON.stringify(favoriteNews));
 
