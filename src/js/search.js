ce6fe667dee303298629e5934df7fbd1e4288e6c
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-03-28 15:19:22 +0300

    Update search.js

--- src/js/search.js
+++ src/js/search.js
@@ -1,11 +1,18 @@
-import throttle from 'lodash.throttle';
 import { createPagination } from './pagination'
 
 const srearchRef = document.querySelector('.search')
-console.log(srearchRef);
+
 
 srearchRef.addEventListener('submit', (e)=>{
     e.preventDefault();
-    const valueForSearch = e.currentTarget.elements.search.value;  
-    createPagination.search(valueForSearch)
+    const valueForSearch = e.currentTarget.elements.search.value;
+    createPagination.search(valueForSearch, getDate());  
 });
+
+function getDate(){
+    const data = JSON.parse(localStorage.getItem('date'));
+    if(!data){
+        return null;
+    }
+    return data.replaceAll('/', '')
+}
