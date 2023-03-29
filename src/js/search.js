84e437740c2a4f09111beded578c5ee2c82087f6
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-03-29 15:47:36 +0300

    delete/disabled-style

--- src/js/search.js
+++ src/js/search.js
@@ -1,12 +1,10 @@
 import { createPagination } from './pagination'
 
 const srearchRef = document.querySelector('.search')
-const categoriesButton  = document.querySelectorAll('.categories__button');
 srearchRef.addEventListener('submit', (e)=>{
     e.preventDefault();
     const valueForSearch = e.currentTarget.elements.search.value;
     createPagination.search(valueForSearch, getDate());
-    categoriesButton.forEach((element)=>element.disabled = true)
 });
 
 export function getDate(){
